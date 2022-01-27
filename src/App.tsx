8228fab59aad2f562e4a7201fe7a9507643e3de3
ollie <oliver.lawrence169@gmail.com>
2022-01-27 19:03:27 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider, Heading, Center, Input, Box, Button, Text } from '@chakra-ui/react'
+import Message from "./Message"
+
 
 function App() {
+  // 2. Use at the root of your app
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChakraProvider>
+      <Center>
+        <Box>
+          <Heading>ChakraUI Heading</Heading>
+          <Message/>
+          <Message/>
+          <Input onChange={(e) => console.log(e.target.value)}/>
+          <Button onClick={() => console.log("Button clicked")}>Send Message</Button> 
+        </Box>
+      </Center>
+    </ChakraProvider>
+  )
 }
-
 export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react'
+
+
+function Message() {
+    return (
+        <Box backgroundColor="lightgray">
+            <Text>Message</Text>
+        </Box>
+    )
+}
+
+export default Message
